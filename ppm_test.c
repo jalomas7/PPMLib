a8e7e1956479a4e10647b8349e2e453d88efdcfa
Jacob Massengill <jacob_massengill@yahoo.com>
2017-02-20 19:24:50 -0500

    reformatted license header

--- ppm_test.c
+++ ppm_test.c
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 //this file generates a 500x500 white on black x
 //and outputs it to X.ppm
 #include "ppm.h"
